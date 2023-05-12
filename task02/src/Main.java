179d709c06fbea7e4f00037345796e7981d18e65
daviniaaa <daviniaho99@gmail.com>
2023-05-12 10:22:55 +0800

    third commit, task02 plan

---
+++ task02/src/Main.java
@@ -0,0 +1,24 @@
+package task02;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // loading
+        // load the folder (.../texts) -> listFiles() into ARRAY 
+        // -> for each item check if it is directory or file -> if file, analyse /
+        // if directory, new path name? until it reaches files
+
+        // analysing
+        // maybe ClientHandler? thread pool?
+        // read file: FileReader -> BufferedReader -> String/Builder?
+        // parse String/Builder: for/while loop -> (i) (i+1)
+        // store data: HashMap<String, HashMap<String, int>> 
+        // -> keyword (i) and list of next words (i+1)
+
+        // printing
+        // for each key, print (innerKey : innerKeySet()) + (innerValue / innerSize())
+        // key
+        // "\t" <innerKey> <innerValue / innerSize>
+        
+    }
+}
